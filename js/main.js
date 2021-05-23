741baf05171492c207929f1732e19c50caa092f0
404FNC <78044349+404FNC@users.noreply.github.com>
2021-05-23 12:15:45 -0400

    Fixes for First Draft
    - a fix that Cruz mentioned to adjust spacing between Our Games and the render slider
    -- adjustment to the cover image - it will be centered upon resizing the page instead of sticking to the top
    - featured.html page has 2 items but the spacing / random thin line is now gone

--- js/main.js
+++ js/main.js
@@ -24,3 +24,20 @@ $('.counting').each(function() {
 
 
 });
+
+// external js: flickity.pkgd.js
+
+var $carousel = $('.carousel').flickity({
+  cellSelector: 'img',
+  imagesLoaded: true,
+  percentPosition: false,
+  wrapAround: true,
+});
+var $caption = $('.caption');
+// Flickity instance
+var flkty = $carousel.data('flickity');
+
+$carousel.on( 'select.flickity', function() {
+  // set image caption using img's alt
+  $caption.text( flkty.selectedElement.alt )
+});
